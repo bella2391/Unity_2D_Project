26940976c0e16a1641c667bbe679966c7b837131
bella <maekawatakaya10.8@gmail.com>
2024-09-11 17:23:22 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/Scripts/SpriteColorChange.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SpriteColorChanger : MonoBehaviour {
+    public Color fillColor = Color.white; // 塗りつぶす色を設定します
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start() {
+        // SpriteRendererコンポーネントを取得します
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // スプライトの色を変更します
+        spriteRenderer.color = fillColor;
+    }
+}
