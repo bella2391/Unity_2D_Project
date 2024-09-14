6df23614d58ba3b6265d5fc2fd7232fe1bda13bd
bella <maekawatakaya10.8@gmail.com>
2024-09-15 03:58:22 +0900

    ボール消失&生成

--- Assets/Scripts/goll.cs
+++ Assets/Scripts/goll.cs
@@ -1,30 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class goll : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject ballPrefab; // ボールのプレハブ
+    public Transform spawnPoint;  // ボールを生成する位置
+
     void Start()
     {
-        
+        // 初期化処理があればここに書きます
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 更新処理があればここに書きます
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "left_goal") {
-
-            Destroy(this.gameObject);
-            Change
-        　
-        
+        // 衝突したオブジェクトの名前が "left_goal" の場合
+        if (collision.gameObject.name == "central_ball")
+        {
+            Destroy(collision.gameObject); // 衝突したボールを破棄
+            SpawnBall(); // 新しいボールを spawnPoint の位置に生成
         }
     }
 
+    void SpawnBall()
+    {
+        if (ballPrefab != null && spawnPoint != null)
+        {
+            // ボールを生成
+            Instantiate(ballPrefab, spawnPoint.position, Quaternion.identity);
+            Debug.Log("Ball spawned at: " + spawnPoint.position);
+        }
+        else
+        {
+            Debug.LogWarning("Ball prefab or spawn point is not assigned.");
+        }
+    }
 }
