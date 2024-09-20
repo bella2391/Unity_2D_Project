dec9deedb1a013d39ef862fc4464206a79a9f9ee
bella <maekawatakaya10.8@gmail.com>
2024-09-20 13:31:49 +0900

    真ん中のボールと境界線を通過させる

--- Assets/Scripts/ball.cs
+++ Assets/Scripts/ball.cs
@@ -2,45 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ball : MonoBehaviour
-{
+public class ball : MonoBehaviour {
     public Transform spawnPoint;  // ボールを生成する位置
     private Rigidbody2D rb;
-    // Start is called before the first frame update
-    void Start()
-    {
+    
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
 
-        if (collision.gameObject.name == "left_goal")///左にゴールしたとき
-        {
-
-
-
-            transform.position = spawnPoint.position;///位置を０００に変更
+        if (collision.gameObject.name == "left_goal") { // 左にゴールしたとき
+        
+            transform.position = spawnPoint.position; // 位置を０００に変更
 
             Vector2 movement = new Vector2(0, 0) ;
             rb.velocity = movement;
-
         }
-                if (collision.gameObject.name == "right_goal")///右にゴールしたとき
-        {
 
-
-
-            transform.position = spawnPoint.position;///位置を０００に変更
+        if (collision.gameObject.name == "right_goal") { // 右にゴールしたとき
+        
+            transform.position = spawnPoint.position; // 位置を０００に変更
 
             Vector2 movement = new Vector2(0, 0) ;
             rb.velocity = movement;
-
         }
     }
 }
---
+++ Assets/Scripts/boundary.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boundary : MonoBehaviour {
+    public LayerMask playerBallLayer, centerBallLayer;
+    private int playerBallLayerID, centerBallLayerID, boundaryLayerID;
+    private bool firstCollided;
+
+    void Start() {
+        // レイヤーIDの取得
+        playerBallLayerID = LayerMask.NameToLayer("PlayerBall");
+        centerBallLayerID = LayerMask.NameToLayer("CenterBall");
+        boundaryLayerID = LayerMask.NameToLayer("Boundary");
+        // レイヤーIDの確認（デバッグ用）
+        Debug.Log("PlayerBall Layer ID: " + playerBallLayerID);
+        Debug.Log("CenterBall Layer ID: " + centerBallLayerID);
+        Debug.Log("Boundary Layer ID: " + boundaryLayerID);
+
+        // 最初だけレイヤー間の衝突判定を無効化
+        Physics2D.IgnoreLayerCollision(playerBallLayerID, boundaryLayerID, true);
+        Physics2D.IgnoreLayerCollision(centerBallLayerID, boundaryLayerID, true);
+        // 衝突判定を無効化するコルーチンを開始
+        // StartCoroutine(DisableCollisionInitially());
+
+        //firstCollided = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    // 初期化のために少し待つコルーチン
+    /*private IEnumerator DisableCollisionInitially() {
+        yield return new WaitForSeconds(0.1f);  // 0.1秒待機
+
+        // 真ん中のボールと境界線の衝突を無効化
+        Physics2D.IgnoreLayerCollision(centerBallLayerID, boundaryLayerID, true);
+    }*/
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+        int collidedLayer = collision.gameObject.layer;
+
+        if (collidedLayer == playerBallLayerID && !firstCollided) {
+            firstCollided = true; // 初衝突のフラグをtrueにセット
+            Debug.Log("プレイヤーボールが真ん中のボールに触れました。衝突判定を有効化します。");
+
+            // 真ん中のボールと境界線の衝突を有効化
+            Physics2D.IgnoreLayerCollision(centerBallLayerID, boundaryLayerID, false);
+        }
+
+        if (collidedLayer == playerBallLayerID) {
+            // プレイヤーのボールが境界線に衝突した場合、通過させない
+            Debug.Log("Player ball hit the boundary!");
+            // プレイヤーのボールと境界線は衝突するように設定
+            Physics2D.IgnoreLayerCollision(playerBallLayerID, boundaryLayerID, false);
+            // 必要に応じて反応を制御
+        } else if (collidedLayer == centerBallLayerID) {
+            // 中央のボールが境界線に衝突した場合、通過させる
+            Debug.Log("Center ball passed the boundary.");
+            // 中央のボールと境界線は衝突しないように設定
+            Physics2D.IgnoreLayerCollision(centerBallLayerID, boundaryLayerID, true);
+        }
+    }
+}
